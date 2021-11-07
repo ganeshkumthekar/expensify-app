561dcdd2342821aff308de09e5aaf41e4afeefe6
Ganesh <ganesh_cis@yahoo.com>
2021-11-06 20:20:21 -0700

    removed test folder

--- src/tests/setupTests.js
+++
@@ -1,9 +0,0 @@
-import Enzyme from 'enzyme';
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
-import { configure } from '@testing-library/react';
-
-Enzyme.configure({ 
-    adapter: new Adapter()
- });
-
- export default configure();
